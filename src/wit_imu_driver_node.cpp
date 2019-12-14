ade8794c27df018c16feb4e0c4e64f4d9ceca0f8
strv <strv.analoger@gmail.com>
2019-12-14 21:02:28 +0900

    initial commit

---
+++ src/wit_imu_driver_node.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright(c) 2019, strv
+ * All rights reserved.
+ */
+
+#include <ros/ros.h>
+#include <ros/console.h>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "wit_imu_driver");
+    return 0;
+}
